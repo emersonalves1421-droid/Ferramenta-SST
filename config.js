45bffc8004be83b10f4c6adc9acf0026b926ea69
Emerson Alves Pereira <emersonalves1421@gmail.com>
2026-09-25 00:32:49 -0300

    adicionando credenciais

---
+++ config.js
@@ -0,0 +1,6 @@
+// Credenciais do Supabase - fica FORA do index.html
+// Assim voce nao precisa editar isso de novo toda vez que trocar o index.html.
+window.SST_CONFIG = {
+  SUPABASE_URL: 'https://sbfmbrdoqeszgjhgeaqs.supabase.co',
+  SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_yBVydjDKzfqXms5nJ7yPgA_2__0lNp-'
+};
